b9e227aca4843f7b2ffdc3f17fea09ea817014c4
Janis Rasins <janis.rasins.accenture@gmail.com>
2022-07-06 22:01:33 +0300

    Initial release 0.1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "./Header/Header"
+import Main from "./Main/Main"
+
+const App = () => {
+	return (
+		<React.Fragment>
+			<Header />
+			<Main />
+		</React.Fragment>
+	)
+}
+
+export default App
